afd3c3019ad658db4b18d3959112950ca4c6ea8a
yosukeend <weareonthecrew@gmail.com>
2023-03-29 10:14:24 +0900

    書籍の削除ができるように変更しました。 また、Listpageコンポーネントで変数名の変更、インデントの整形をしました。

--- intern-book-app/src/app/components/card/card.component.ts
+++ intern-book-app/src/app/components/card/card.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Book } from '../../types/ book';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-  //変数名をbookShowからbookに変更
   @Input() book: Book = {
     name: '',
     detail: '',
-    evaluation: 0
+    evaluation: 0,
+  };
+
+  @Output() onDelete = new EventEmitter();
+
+  _onDelete() {
+    this.onDelete.emit();
   }
 }
--- intern-book-app/src/app/pages/listpage/listpage.component.ts
+++ intern-book-app/src/app/pages/listpage/listpage.component.ts
@@ -1,29 +1,63 @@
 import { Component } from '@angular/core';
 import { Book } from '../../types/ book';
 
-
 @Component({
   selector: 'app-listpage',
   templateUrl: './listpage.component.html',
-  styleUrls: ['./listpage.component.css']
+  styleUrls: ['./listpage.component.css'],
 })
 export class ListpageComponent {
-  addToBookList: Book = {
+  bookForm: Book = {
     name: '',
     detail: '',
-    evaluation: 0
+    evaluation: 0,
   };
 
   bookList: Book[] = [
-    {name: '呪術廻戦', detail: '呪い。辛酸・後悔・恥辱....。人間の負の感情から生まれる。禍々しきその力は、人を死へと導く。', evaluation: 10},
-    {name: '怪獣8号', detail: '怪獣発生率が世界屈指の日本。この国は、容赦無く怪獣が日常を侵していた。', evaluation: 9},
-    {name: 'アオサシ', detail: '愛媛に暮らす中学3年生。将来日本のサッカーに革命を起こすことになる少年の運命は ！？', evaluation: 8},
-    {name: '株式会社マジルミエ', detail: '"魔法少女"---それは自然災害の一種「怪異」の退治業務を請け負う職業。', evaluation: 7},
-    {name: 'SPYxFAMILY', detail: '世界核国が表面化で熾烈な情報戦を繰り広げていた時代。世界の平和は託された---。', evaluation: 9},
-    {name: '金色のガッシュ！！', detail: '魔界の王を決めるため、100名の魔物の子供達が人間とコンビを組み、最後の一組になるまで戦う。', evaluation: 7}
+    {
+      name: '呪術廻戦',
+      detail:
+        '呪い。辛酸・後悔・恥辱....。人間の負の感情から生まれる。禍々しきその力は、人を死へと導く。',
+      evaluation: 10,
+    },
+    {
+      name: '怪獣8号',
+      detail:
+        '怪獣発生率が世界屈指の日本。この国は、容赦無く怪獣が日常を侵していた。',
+      evaluation: 9,
+    },
+    {
+      name: 'アオサシ',
+      detail:
+        '愛媛に暮らす中学3年生。将来日本のサッカーに革命を起こすことになる少年の運命は ！？',
+      evaluation: 8,
+    },
+    {
+      name: '株式会社マジルミエ',
+      detail:
+        '"魔法少女"---それは自然災害の一種「怪異」の退治業務を請け負う職業。',
+      evaluation: 7,
+    },
+    {
+      name: 'SPYxFAMILY',
+      detail:
+        '世界核国が表面化で熾烈な情報戦を繰り広げていた時代。世界の平和は託された---。',
+      evaluation: 9,
+    },
+    {
+      name: '金色のガッシュ！！',
+      detail:
+        '魔界の王を決めるため、100名の魔物の子供達が人間とコンビを組み、最後の一組になるまで戦う。',
+      evaluation: 7,
+    },
   ];
 
-  saveToBookList(): void {
-    this.bookList.push({name: this.addToBookList.name, detail: this.addToBookList.detail, evaluation: this.addToBookList.evaluation});
+  addToBookList(): void {
+    //値渡し スプレッド構文
+    this.bookList.push({ ...this.bookForm });
+  }
+
+  deleteFromBookList(index: number): void {
+    this.bookList.splice(index, 1);
   }
 }
